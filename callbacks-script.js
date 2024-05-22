61c124b4c3c8afef9575394824c102ad8480e737
AbhinayNegi <negia394@gmail.com>
2024-05-22 12:12:27 +0530

    Completed the Callback task

--- callbacks-script.js
+++ callbacks-script.js
@@ -1,8 +1,31 @@
-const callbackBtn = document.getElementsByTagName("button")[0];
-const div = document.getElementsByClassName("update-text")[0];
+const button = document.getElementsByTagName("button")[0];
+const callbackMsg = document.getElementById("callBackMsg");
+const containerDiv = document.getElementsByClassName("container")[0];
 
-callbackBtn.addEventListener("click", updateText);
+button.addEventListener("click", fetchData);
+function fetchData() {
 
-function updateText(event) {
-    setTimeout(() => div.innerHTML = "Callback executed after 5 seconds", 5000);
+    setTimeout(displayData, 5000);
+}
+
+function displayData() {
+
+    callbackMsg.innerHTML = "Callback executed after 5 seconds"
+
+    let data = fetch("https://dummyjson.com/posts");
+
+    const heading = document.createElement("h2");
+    heading.innerHTML = "POST TITLES";
+    containerDiv.appendChild(heading);
+    
+    data.then(response => response.json())
+    .then(data => {
+        const posts = data.posts;
+        posts.forEach(post => {
+            let postDiv = document.createElement("div");
+            postDiv.setAttribute("class", "post-titles");
+            postDiv.innerHTML = post.title;
+            containerDiv.appendChild(postDiv);
+        })
+    })
 }

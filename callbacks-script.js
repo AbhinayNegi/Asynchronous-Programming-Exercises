340bfac8a2bacdb9539ffa81e68ffc371ca4e699
AbhinayNegi <negia394@gmail.com>
2024-05-22 13:06:33 +0530

    Added comments

--- callbacks-script.js
+++ callbacks-script.js
@@ -9,18 +9,23 @@ function fetchData() {
 }
 
 function displayData() {
-
+    
     callbackMsg.innerHTML = "Callback executed after 5 seconds"
 
     let data = fetch("https://dummyjson.com/posts");
 
+    //Creating heading for the posts
     const heading = document.createElement("h2");
     heading.innerHTML = "POST TITLES";
     containerDiv.appendChild(heading);
     
+    // Getting the response and converting the json data
     data.then(response => response.json())
     .then(data => {
+        // Getting array of posts
         const posts = data.posts;
+        // For every post  we are creating a div element adding class to its attribute
+        // And setting its inner html to the post title then appending the div to container
         posts.forEach(post => {
             let postDiv = document.createElement("div");
             postDiv.setAttribute("class", "post-titles");

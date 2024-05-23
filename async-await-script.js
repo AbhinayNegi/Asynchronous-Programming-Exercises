07d73c8ef3d4f854f07bcd8d8a9aef2b14493011
AbhinayNegi <negia394@gmail.com>
2024-05-23 13:17:34 +0530

    Completed JS for async await task

--- async-await-script.js
+++ async-await-script.js
@@ -5,20 +5,39 @@ const dataDiv = document.getElementsByClassName("data")[0];
 
 button.addEventListener("click", getData);
 async function getData(event) {
-    data = await fetchData();
-    console.log(data);
+    try {
+        data = await fetchData();
+        callbackMsg.innerHTML = "Data fetched within 5 seconds.";
+        // Getting array of posts
+        const posts = data.posts;
+        // For every post  we are creating a div element adding class to its attribute
+        // And setting its inner html to the post title then appending the div to container
+        posts.forEach((post) => {
+            let postDiv = document.createElement("div");
+            postDiv.setAttribute("class", "post-titles");
+            postDiv.innerHTML = post.title;
+            dataDiv.appendChild(postDiv);
+        })
+    } catch (error) {
+        callbackMsg.innerHTML = error;
+    }
+
 }
 
 async function fetchData() {
-    let timeout;
-    const timeoutPromise = new Promise((resolve, reject) => {
-        timeout = setTimeout(() => {
-            reject("Could not fetch data within 5 seconds");
-        }, 5000);        
-    });
+    // Sets a timeout to reject the promise after 5 seconds if data fetch fails.
+    const timeoutPromiseStatus = new Promise((resolve, reject) => {
+        setTimeout(() => reject("Could not load data within 5 minutes"), 5000);
+    })
 
-    let fetchedData = await fetch("https://fakeresponder.com/?sleep=7000");
-    let converted = await fetchedData.json();
-    clearTimeout(timeout);
-    return converted;
+    // Fetches data from the specified URL and converts it to a JavaScript object
+    const fetchedData = fetch("https://dummyjson.com/posts")
+        .then(response => response.json())
+        .then(data => data);
+
+    // We are calling Promise.race function that takes array of promises 
+    // and whichever promise resolves or reject first it will create another promise for that and returns it
+    // If fetch API taking longer than 5 seconds that in that case the timeout callback will run and reject the promise and then the Promise.race will also return promise with reject status.
+    // If fetch API fetches the data before 5 seconds then Promise.race returns promise with resolved status
+    return Promise.race([timeoutPromiseStatus, fetchedData]);
 }
